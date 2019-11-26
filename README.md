df3aac0b8fe81313d0b0d0aba99538c695493f0b
TonhaoSantos <ahssantos1@gmail.com>
2019-11-26 17:35:28 -0300

    Refactoring, License, New Version and Readme Update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+![GitHub package.json version](https://img.shields.io/github/package-json/v/lipecss/realtime-current-location)
+[![GitHub issues](https://img.shields.io/github/issues/lipecss/realtime-current-location)](https://github.com/lipecss/realtime-current-location/issues)
+[![GitHub forks](https://img.shields.io/github/forks/lipecss/realtime-current-location)](https://github.com/lipecss/realtime-current-location/network)
+[![GitHub stars](https://img.shields.io/github/stars/lipecss/realtime-current-location)](https://github.com/lipecss/realtime-current-location/stargazers)
+[![GitHub license](https://img.shields.io/github/license/lipecss/realtime-current-location)](https://github.com/lipecss/realtime-current-location)
+
 # Current Location
 This will render a map with your location, perfectly centered horizontally and vertically with a box contains all your informations
 
@@ -5,9 +11,14 @@ This will render a map with your location, perfectly centered horizontally and v
 
 ### Install
 
-```shell
+```js
+// npm
 $ npm install
 $ npm start
+
+// yarn
+$ yarn
+$ yarn start
 ```
 
 ### License
--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-const axios = require('axios');
+const axios = require('axios')
 const geoip = require('geo-from-ip')
 const blessed = require('blessed')
-   , contrib = require('blessed-contrib')
-   , screen = blessed.screen()
-   , map = contrib.map({label: 'World Map'})
+const contrib = require('blessed-contrib')
+const screen = blessed.screen()
+const map = contrib.map({ label: 'World Map' })
 
-   screen.title = 'My Location'
+screen.title = 'My Location'
 
-axios.get('http://ip-api.com/json/')
-.then(function (response) {
-  CreateMap(response.data)
-  })
-.catch(function (error) {
-  console.log(error);
-});
-
-function CreateMap(local){
+function CreateMap(local) {
   const location = geoip.allData(`${local.query}`)
   const long = location.location.longitude
   const lat = location.location.latitude
 
-  var grid = new contrib.grid({rows: 80, cols: 60, screen: screen})
+  let grid = new contrib.grid({
+    rows: 80,
+    cols: 60,
+    screen: screen
+  })
 
   screen.append(map)
-  map.addMarker({"lon" : long, "lat" : lat, color: "red", char: "X" })
-  var box = grid.set(55, 25, 26, 25, blessed.box, {content: 'My Location'})
+
+  map.addMarker({
+    'lon': long,
+    'lat': lat,
+    'color': 'red',
+    'char': 'X'
+  })
+
+  let box = grid.set(55, 25, 26, 25, blessed.box, { content: 'My Location' })
 
   box.setContent(`MY IP ${local.query}\n\nState: ${location.state}\nCity: ${location.city}\nCountry: ${location.country}\nContinent:${location.continent}\nLongitude: ${long}\nLatitude:${lat}`);
 
@@ -32,8 +35,10 @@ function CreateMap(local){
 }
 
 
-
-
-
-
-
+axios.get('http://ip-api.com/json/')
+  .then((response) => {
+    CreateMap(response.data)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
